70c91d27797623a83ef594c9f39ff68f5f9b6794
aliyassine7 <aliyassine43@gmail.com>
2024-05-12 14:01:28 -0500

    added new lines

--- cypress/e2e/integration/05-assertions.cy.js
+++ cypress/e2e/integration/05-assertions.cy.js
@@ -231,15 +231,118 @@ describe("Assertions", () => {
      * 3. Validate their texts with expected text
      * 4. Validate these elements are visible.
      */
-  });
 
-  it.only('Practice Problem', () =>  {
-    const arr = ['Programming Languages', 'Automation Tools'];
+    const arr2 = ["Programming Languages", "Automation Tools"];
 
     cy.get('[data-identifier="Headings"] > h4').each(($el, index) => {
+      cy.wrap($el).should("have.text", arr2[index]).and("be.visible");
+    });
+
+    const switch_case = "empty";
+
+    // switch (switch_case) {
+    //   case "non-empty": {
+    //     cy.log("blabla");
+    //     break;
+    //   }
+
+    //   case "1non-empty":
+    //   case "2non-empty": {
+    //     cy.log("Tech");
+    //     break;
+    //   }
+
+    //   case "full": {
+    //     cy.log("Global");
+    //     break;
+    //   }
+
+    //   default:{
+    //     throw new Error(`My error: ${switch_case} is missing `);
+    //   }
+    // }
+
+    /**
+     * 1. On the Html Elements page
+     * 2. From the "Checkboxes" section, locate all checkboxes
+     * 3. Validate their texts with expected text
+     * 4. Validate checkboxes are visible, and enabled
+     */
+
+    const exp = ["Apple", "Microsoft", "Tesla"];
+
+    cy.get("#checkbox-button-group div").each(($el, index) => {
+      cy.wrap($el).find("label").should("have.text", exp[index]);
+      // .parent().find('input').should('be.visible').and('be.enabled')
+
+      cy.wrap($el).find("input").should("be.visible").and("be.enabled");
+    });
+  });
+
+  it.only("Asserion Practices", () => {
+    /**
+     * 1. Go to https://techglobal-training.com/frontend
+     * 2. Navigate to 'Html Elements' card
+     * 3. From the "Text Inputs" section
+     * 4. Validate text input 1 and text input 2 is enabled
+     * 5. Validate text input 1 and text input 2 is not required
+     * 6. Enter your name and last name
+     */
+
+    const names = ["Tech", "Global"];
+
+    cy.get("#text_input1, #text_input2").each(($el, index) => {
       cy.wrap($el)
-      .should('have.text', arr[index])
-      .and('be.visible')
-    })
-  })
+        .type(names[index])
+        .should("be.enabled")
+        .and("not.have.attr", "required");
+
+      /**
+       * 1. Go to https://techglobal-training.com/frontend
+       * 2. Navigate to 'Html Elements' card
+       *
+       * 3. From the "Date Inputs" section
+       * 4. Validate date input 1 and date input 2 is enabled
+       * 5. Validate date input 1 and date input 2 is is not required
+       * 6. Enter dates for both date input 1 and date input 2
+       * 7. Validate value is changed to given dates.
+       */
+
+      const dates = ["01/01/2024", "01/01/2024"];
+
+      cy.get("#date_input1, #date_input2").each(($el, index) => {
+        cy.wrap($el)
+          .clear()
+          .type(`${dates[index]}{enter}`)
+          .should("have.value", dates[index])
+          .and("be.enabled")
+          .and("not.have.attr", "required");
+      });
+
+      /**
+       * 1. Go to https://techglobal-training.com/frontend
+       * 2. Navigate to 'Html Elements' card
+       * 3. From the "Dropdowns" section
+       * 4. Validate dropdown 1 and dropdown 2 is enabled
+       * 6. Enter Microsoft for dropdown 1 and Apple for dropdown 2
+       * 7. Validate options are selected
+       */
+
+      const dropdowns = ["Microsoft", "Apple"];
+
+      cy.get("#company_dropdown1, #company_dropdown2").each(($el, index) => {
+        cy.wrap($el)
+          .select(dropdowns[index])
+          .should("be.enabled")
+          .and("have.value", dropdowns[index]);
+      });
+
+      cy.get("#company_dropdown1, #company_dropdown2").each(($el, index) => {
+        cy.wrap($el)
+          .select(dropdowns[index])
+          .find("option:selected")
+          .should("have.text", dropdowns[index]);
+      });
+    });
+  });
 });
