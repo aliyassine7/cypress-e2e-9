0b0ce65252baae1d0a3e7e5274059648b0b647f1
aliyassine7 <aliyassine43@gmail.com>
2024-05-13 20:20:03 -0500

    added new lines

--- cypress/e2e/practices/practice01.cy.js
+++ cypress/e2e/practices/practice01.cy.js
@@ -1,14 +1,29 @@
-/// <reference types="cypress"/>
+beforeEach(() => {
+  cy.visit('https://www.google.com/');
+});
 
-describe('Practice01 - Validate Google Home Page', () => {
-
-  beforeEach(() => {
-    cy.visit('https://www.google.com/');
-  });
-  
+describe('Practice01 Validate Google Home Page', () => {
   it('TASK-1: Validate the Google Home Page Title and URL', () => {
-      
+    cy.title().should('equal', 'Google');
+    
+    cy.url().should('eq', 'https://www.google.com/')
   });
 
+  it('TASK-2: Validate the Google Logo Presence', () => {
+    cy.get('.lnXdpd').should('be.visible');
+  })
 
+  it('TASK-3: Validate the Google Search Results', () => {
+    cy.get('.gLFyf').click().type('cypress{enter}');
+
+    cy.get('#APjFqb').should('be.visible');
+  });
+
+  it('TASK-4: Validate the Google Search Autocomplete Suggestions', () => {
+    cy.get('.gLFyf').click().type('Artificial Intelligence');
+
+    cy.get('#jZ2SBf').should('be.visible').click();
+
+    cy.url().should('contain','q=artificial+intelligence', { matchCase: false });
+  });
 });
