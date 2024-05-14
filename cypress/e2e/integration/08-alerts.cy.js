ee99b9b117acb3a0419d1ed134c40663d29e875e
aliyassine7 <aliyassine43@gmail.com>
2024-05-13 21:00:40 -0500

    added alerts

---
+++ cypress/e2e/integration/08-alerts.cy.js
@@ -0,0 +1,16 @@
+describe("Handling Alerts", () => {
+  beforeEach(() => {
+    cy.visit("https://www.techglobal-training.com/frontend/");
+    cy.clickCard("Alerts");
+  });
+
+  it("Handling the Warning Alert", () => {
+    
+    cy.on("window:alert", (str) => {
+      console.log(`My warning alert text content is: ${str}`);
+      expect(str).equal("You are on TechGlobal Training application.");
+    });
+
+    cy.get("#warning_alert").click();
+  });
+});
