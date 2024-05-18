ac1320dcf6c7e8d648dde3cb629f4eee8172b7f7
aliyassine7 <aliyassine43@gmail.com>
2024-05-18 12:00:37 -0500

    added new dependencies

---
+++ cypress/e2e/integration/09-advancedActions.cy.js
@@ -0,0 +1,9 @@
+describe("Keyboard", () => {
+  beforeEach(() => {
+    cy.visit("https://www.techglobal-training.com/frontend/");
+    cy.clickCard("Html Elements");
+  });
+
+  it("Handling the Warning Alert", () => {
+  });
+});
