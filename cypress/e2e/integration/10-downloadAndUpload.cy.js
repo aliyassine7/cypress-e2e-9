c3329a7bfa1975e077eb912d1e215daad3b648d1
aliyassine7 <aliyassine43@gmail.com>
2024-05-19 10:23:31 -0500

    new lines

--- cypress/e2e/integration/10-downloadAndUpload.cy.js
+++ cypress/e2e/integration/10-downloadAndUpload.cy.js
@@ -6,11 +6,33 @@ describe("File Download & File Upload", () => {
     cy.clickCard("File Download & Upload");
   });
 
+  let fileName;
+
   it("File Download", () => {
-    cy.get('#file_download').click()
+    cy.get("#file_download").click();
 
-    const fileName = 'SampleText.txt';
+    fileName = "SampleText.txt";
 
-    cy.readFile(`cypress/downloads/${fileName}`)
-  })
+    cy.readFile(`cypress/downloads/${fileName}`);
+  });
+
+  /**
+   * Go to https://techglobal-training.com/frontend/
+   * Click on the "File Upload" card
+   * Send the path of the file as keys to the "Choose File" input box
+   * Click on the "UPLOAD" button
+   * Validate the result message equals "You Uploaded 'SampleText.txt'"
+   */
+
+  it("File Upload", () => {
+    cy.get("#file_upload").selectFile(`cypress/downloads/${fileName}`);
+
+    // cy.get('#file_upload').selectFile([`cypress/downloads/${fileName}`, `cypress/downloads/${fileName}2`])
+
+    // cy.get('#file_upload').selectFile(`cypress/downloads/${fileName}`, { action: 'drag-drop'})
+
+    cy.get('#file_submit').realClick()//.click()
+
+    cy.get('#result').should('have.text', `You uploaded ${fileName}`)
+  });
 });
