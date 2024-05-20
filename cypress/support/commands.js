858d5904aba63690d9fbc75961704055fa62c673
aliyassine7 <aliyassine43@gmail.com>
2024-05-20 18:25:19 -0500

    new lines

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -11,12 +11,46 @@
 //
 // -- This is a parent command --
 // Cypress.Commands.add('login', (email, password) => { ... })
+
 Cypress.Commands.add("clickCard", (link) => {
   cy.get(".cards").contains(link).click();
 });
+
+Cypress.Commands.add("selectDropdownOption", (element, option) => {
+  cy.get(element).select(option);
+});
+
+Cypress.Commands.add("login", (email, name) => {
+  cy.get('[type="email"]').type(email);
+  cy.get('.mt-3 > [type="text"]').type(name);
+  cy.get(".mt-3 + button").click();
+});
+
+Cypress.Commands.add("haveText", { prevSubject: true }, (subject, value) => {
+  // expect(subject.text()).equal(value);
+  cy.wrap(subject).should("have.text", value);
+});
+
+Cypress.Commands.add("haveText2", (el, value) => {
+  cy.get(element).should("have.text", value);
+});
+
+Cypress.Commands.add("logText", { prevSubject: true }, (subject) => {
+  const text = subject.text();
+
+  cy.log(text);
+
+  return cy.wrap(subject);
+});
+
 //
 // -- This is a child command --
 // Cypress.Commands.add('drag', { prevSubject: 'element'}, (subject, options) => { ... })
+
+Cypress.Commands.add('assertAttribute', {prevSubject: true}, (subject, attribute, value) => {
+  cy.wrap(subject).should('have.attr', attribute, value)
+});
+
 //
 //
 // -- This is a dual command --
